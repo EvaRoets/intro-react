4d17de2f7269539967ede6a8c906857baeff0b1a
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-30 16:02:33 +0200

    set default todo's with props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+// Import state hook for function component to be rendered every time it changes
+import React, { useState} from "react";
 import React from 'react';
 import TodoList from "./TodoList";
 
 //Access root of entire application
 function App() {
+   // Object destructuring: Use default array for to-dos, saved in const (all to-dos, function to update to-dos)
+    const [todos, setTodos] = useState(['To do 1', 'To do 2', 'To do 3'])
   //Add html for to-do app
   return (
-    // fragment, allowing to add multiple JSX elements in 1 function
+    // empty component = fragment, allowing to add multiple JSX elements in 1 function
     <>
-      {/*1st JSX element*/}
-      <TodoList/>
+      /*1st JSX element with passed props todos ~ html attribute */>
+      <TodoList todos = {todos}/>
       <input type="text"/>
       <button>Add To-do</button>
       <button>Clear Completed To-dos</button>
-      <div>0 left to do</div>
+      <div>0 left To-do</div>
     </>
   )
 
