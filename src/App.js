856aa260fbc29d6ca68fe8e1c5c0905abeb726cb
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-30 14:55:59 +0200

    Add TodoList function component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 
+//Access root of entire application
 function App() {
-  return null
-  // (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React @ BeCode
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+  //Add html for to-do app
+  return (
+    //1st JSX element
+    <TodoList/>
+  )
+
 }
 
 export default App;
