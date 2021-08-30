d43f2acc38cada2f1ae31ac044299089123f5d62
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-30 16:42:25 +0200

    Add unique keys to todos to only render changed todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,8 +4,9 @@ import Todo from './Todo'
 export default function TodoList({ todos }) {
     return (
         // loop over todos with .map
+        // add keys to todos ~ htlm id
         todos.map(todo => {
-            return <Todo todo={todo} />
+            return <Todo key ={todo} todo={todo} />
         })
     )
 }
