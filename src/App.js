f6bea13bc6bb187d09a07bf5e73af2e3d0f1ceed
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-30 15:34:48 +0200

    Add App html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import TodoList from "./TodoList";
 
 //Access root of entire application
 function App() {
   //Add html for to-do app
   return (
-    //1st JSX element
-    <TodoList/>
+    // fragment, allowing to add multiple JSX elements in 1 function
+    <>
+      {/*1st JSX element*/}
+      <TodoList/>
+      <input type="text"/>
+      <button>Add To-do</button>
+      <button>Clear Completed To-dos</button>
+      <div>0 left to do</div>
+    </>
   )
 
 }
