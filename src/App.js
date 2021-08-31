725e4ece76533196009c327202968fa51f9dccd3
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-31 11:55:20 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 // Import state hook for function component to be rendered every time it changes
-import React, { useState} from "react";
+import React, {useState} from "react";
+import './css/style.css';
 import TodoList from "./TodoList";
+import Footer from "./components/footer";
 
 //Access root of entire application
 function App() {
-   // Object destructuring: Use default array for to-dos, saved in const (all to-dos, function to update to-dos)
-    const [todos, setTodos] = useState([{id : 1, name: 'To-do 1', complete: false}])
-  //Add html for to-do app
-  return (
-    // empty component = fragment, allowing to add multiple JSX elements in 1 function
-    <>
-      {/*1st JSX element with passed props todos ~ html attribute */}
-      <TodoList todos = {todos}/>
-      <input type="text"/>
-      <button>Add To-do</button>
-      <button>Clear Completed To-dos</button>
-      <div>0 left To-do</div>
-    </>
-  )
-
+    // Object destructuring: Use default array for to-dos, saved in const (all to-dos, function to update to-dos)
+    const [todos, setTodos] = useState([])
+    //Add html for to-do app
+    return (
+        // empty component = fragment, allowing to add multiple JSX elements in 1 function
+        <>
+            {/*1st JSX element with passed props todos ~ html attribute */}
+            <TodoList todos={todos}/>
+            <input type="text"/>
+            <button>Add To-do</button>
+            <button>Clear Completed To-dos</button>
+            <div>0 left To-do</div>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+    const name = "Eva Roets"
+    return (
+        <>
+            <div className="footer">
+                Copyright &copy;
+                <script>document.write(new Date().getFullYear())</script>
+                {name}
+            </div>
+        </>
+    );
+}
+
+export default Footer;
