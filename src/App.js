965c276ed60924aeae65e236f320229d4d68ccef
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-31 16:17:54 +0200

    Add onclick listener for add to-do

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // Import state hook for function component to be rendered every time it changes
-import React, {useState} from "react";
+// Import use ref hook to access elements in html
+import React, { useState, useRef } from "react";
 import './css/style.css';
 import TodoList from "./TodoList";
 import Footer from "./components/footer";
@@ -8,14 +9,34 @@ import Footer from "./components/footer";
 function App() {
     // Object destructuring: Use default array for to-dos, saved in const (all to-dos, function to update to-dos)
     const [todos, setTodos] = useState([])
+    // Define ref variable with useRef(), allowing access to input tag in function
+    const todoNameRef = useRef()
+
+    //Add function to handle button click
+    function AddTodo (event) {
+        //TODO take previous to-dos
+        //TODO add new to-dos
+        //TODO set new current to-dos
+
+        //Access input tag value
+        const name = todoNameRef.current.value
+        // If there is no to-do,  return so there is no empty to do
+        if (name === '') return
+        console.log (name)
+        todoNameRef.current.value = null
+
+    }
+
     //Add html for to-do app
     return (
         // empty component = fragment, allowing to add multiple JSX elements in 1 function
         <>
             {/*1st JSX element with passed props todos ~ html attribute */}
             <TodoList todos={todos}/>
-            <input type="text"/>
-            <button>Add To-do</button>
+            {/*set ref to a variable so it is accessible through useRef inside function */}
+            <input type="text" ref={todoNameRef}/>
+            {/*Add function to handle the button click*/}
+            <button onClick={AddTodo}>Add To-do</button>
             <button>Clear Completed To-dos</button>
             <div>0 left To-do</div>
             <Footer/>
