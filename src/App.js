b5277a5500a5efe4bda3220a0f1aad63f8264a31
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-01 11:24:09 +0200

    Save to-dos to local storage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 // Import state hook for function component to be rendered every time it changes
 // Import use ref hook to access elements in html
-import React, { useState, useRef } from "react";
+// Import use effect hook to store to-dos in local storage
+import React, { useState, useRef, useEffect } from "react";
 import './css/style.css';
 import TodoList from "./TodoList";
 import Footer from "./components/footer";
-// import uuidv4 from 'uuid/v4'
-
+import { v4 as uuidv4 } from 'uuid'
 
 //Access root of entire application
 function App() {
-    // Object destructuring: Use default array for to-dos, saved in const (all to-dos, function to update to-dos)
+    // Object destructuring: shorthand for
+    // const myState = useState(...); returning the values of the array
+    // const todos = myState[0]; 1st value = current state = current list of to-dos
+    // const setTodos = myState[1]; 2nd value = update current state
     const [todos, setTodos] = useState([])
+
     // Define ref variable with useRef(), allowing access to input tag in function
     const todoNameRef = useRef()
 
+    // Define key in const for useEffect function
+    const localStorageKey = 'todoApp.todos'
+
+
+
+    // Add function to save our to-dos everytime these to-dos change
+    // with 1st parameter effect = another function to do things, 2nd parameter dependencies = array of properties
+    useEffect(() => {
+        localStorage.setItem(localStorageKey, JSON.stringify(todos))
+    }, [todos])
+
+
+
+
     //Add function to handle button click
     function AddTodo (event) {
-        //TODO set new current to-dos
-
         //Access input tag value
         const name = todoNameRef.current.value
         // If there is no to-do,  return so there is no empty to do
@@ -27,9 +43,7 @@ function App() {
         setTodos(prevTodos => {
             return [...prevTodos,
                 //Add new to-dos, with uuidv4 function to generate random IDs
-                // TODO { id: uuidv4(), name: name, complete: false}]
-                { id: 1, name: name, complete: false}]
-
+                { id: uuidv4(), name: name, complete: false}]
         })
 
         // Clear out input field after clicking
